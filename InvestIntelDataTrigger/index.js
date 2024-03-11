fb09bd818b211fb0fe67740808433c7ba8488fd8
soumyamund <soumyaranjan.nitr@gmail.com>
2024-03-12 00:48:44 +0530

    Return lotsize with stock Data

--- InvestIntelDataTrigger/index.js
+++ InvestIntelDataTrigger/index.js
@@ -1,5 +1,7 @@
 var getNifty50LiveDataAsync = require("./nsecalls");
-var jsonUtils =  require( "./jsonutils");
+var jsonUtils = require("./jsonutils");
+const GetLotSizeJsonAsync = require("./localcalls");
+const MergeArray = require("./arrayutils");
 
 module.exports = async function GetLatestStockData(context, req) {
   try {
@@ -8,9 +10,20 @@ module.exports = async function GetLatestStockData(context, req) {
     const formattedResponse = JSON.stringify(
       jsonData.map(jsonUtils.removeSpacesFromKeys)
     );
+
+    const lotsizeJsonData = await GetLotSizeJsonAsync();
+    const liveStockJsonData = JSON.parse(formattedResponse);
+    const combineResponse = MergeArray(
+      liveStockJsonData,
+      lotsizeJsonData,
+      "SYMBOL"
+    );
+    console.log(typeof lotsizeJsonData);
+    console.log(typeof liveStockJsonData);
+
     context.res = {
       status: 200,
-      body: formattedResponse,
+      body: combineResponse,
       headers: {
         "Content-Type": "application/json",
       },
